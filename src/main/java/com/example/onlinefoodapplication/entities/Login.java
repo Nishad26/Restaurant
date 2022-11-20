4c6365469f8d3feabae17d8bbf9dfd4b14d4fc41
Nishad Kadam <nishadkadam55@gmail.com>
2022-11-20 11:53:09 +0530

    first commit

---
+++ src/main/java/com/example/onlinefoodapplication/entities/Login.java
@@ -0,0 +1,31 @@
+package com.example.onlinefoodapplication.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "login")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Login {
+
+	@Id
+	private int userid;
+	private String userName;
+	private String password;
+	private boolean loggedIn;
+	private boolean isOwner;
+
+}
